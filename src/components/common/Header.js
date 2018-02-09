1c87a159e28b44d53d5b59826765e431350d31fa
PatyCu <patycuenca@gmail.com>
2018-02-09 10:37:08 +0100

    created header and app skeleton

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    color: LightSlateGray;
-`;
+import '../../styles/header.scss';
+import {NavLink, Switch, Route} from 'react-router-dom';
+import HomePage from '../home/HomePage';
 
 const Header = () => {
     return (
-       <Title>Hello World</Title>   
+       //<Title>Hello World</Title>   
+        <div>
+            <ul>
+                <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                <li><NavLink to="/Menu_item2" activeClassName="active">Menu 2</NavLink></li>
+                <li><NavLink to="/Menu_item3" activeClassName="active">Menu 3</NavLink></li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+            </Switch>    
+        </div>
     );
 };
 
