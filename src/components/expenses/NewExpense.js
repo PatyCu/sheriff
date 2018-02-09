6522a5e1299afcecedf4730b386f107dbf92810a
PatyCu <patycuenca@gmail.com>
2018-02-09 16:15:57 +0100

    added hiddel field to store the name of the trip

--- src/components/expenses/NewExpense.js
+++ src/components/expenses/NewExpense.js
@@ -5,7 +5,7 @@ import '../../styles/NewExpense.scss';
 const NewExpense = () => {
     return (
         <div className="NewExpense">
-            <iframe title="new_menu" id="typeform-full" src="https://patycu.typeform.com/to/gHvLHD">new expense</iframe>
+            <iframe title="new_menu" id="typeform-full" src="https://patycu.typeform.com/to/gHvLHD?trip_name=donostia">new expense</iframe>
         </div>
     );
 };
